1a52d22156973170e005c0ff44295cd3eff10c41
Zehra Seren <fatmazehraseren@gmail.com>
2025-08-08 00:21:26 +0300

    feat(CardStatistics): implement component to show user card statistics with basic styling

--- StoreFlow/ViewComponents/DashboardComponents/_CardStatisticsComponentPartial.cs
+++ StoreFlow/ViewComponents/DashboardComponents/_CardStatisticsComponentPartial.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using StoreFlow.Context;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Immutable;
 
 namespace StoreFlow.ViewComponents.DashboardComponents;
 
 public class _CardStatisticsComponentPartial : ViewComponent
 {
+    private readonly StoreContext _context;
+
+    public _CardStatisticsComponentPartial(StoreContext context)
+    {
+        _context = context;
+    }
+
     public IViewComponentResult Invoke()
     {
+        ViewBag.totalCustomerCount = _context.Customers.Count();
+        ViewBag.totalCategoryCount = _context.Categories.Count();
+        ViewBag.totalProductCount = _context.Products.Count();
+        ViewBag.avgCustomerBalance = _context.Customers.Average(x=>x.CustomerBalance);
+        ViewBag.totalOrderCount = _context.Orders.Count();
+        ViewBag.sumOrderProductCount = _context.Orders.Sum(x => x.OrderCount);
         return View();
     }
 }
