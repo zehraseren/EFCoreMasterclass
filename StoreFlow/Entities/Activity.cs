07536d47207b789d2f55f5ffb088997dd9e43a65
Zehra Seren <fatmazehraseren@gmail.com>
2025-08-08 22:16:49 +0300

    feat(entity): activity entity

---
+++ StoreFlow/Entities/Activity.cs
@@ -0,0 +1,9 @@
+﻿namespace StoreFlow.Entities;
+
+public class Activity
+{
+    public int ActivityId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public TimeOnly ActivityTime { get; set; }
+}
