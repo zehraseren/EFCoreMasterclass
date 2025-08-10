8d6172331c31d2b33525b4d8f87c7d4c23279cf6
Zehra Seren <fatmazehraseren@gmail.com>
2025-08-10 16:10:24 +0300

    feat(dashboard): implement two partial components (SalesData, Todo)

---
+++ StoreFlow/ViewComponents/DashboardComponents/_SalesDataComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using StoreFlow.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreFlow.ViewComponents.DashboardComponents;
+
+public class _SalesDataComponentPartial : ViewComponent
+{
+    private readonly StoreContext _context;
+
+    public _SalesDataComponentPartial(StoreContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var result = _context.Orders.OrderByDescending(x => x.OrderId).Include(y => y.Customers).Include(z => z.Products).Take(5).ToList();
+        return View(result);
+    }
+}
---
+++ StoreFlow/ViewComponents/DashboardComponents/_TodoComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using StoreFlow.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreFlow.ViewComponents.DashboardComponents;
+
+public class _TodoComponentPartial : ViewComponent
+{
+    private readonly StoreContext _context;
+
+    public _TodoComponentPartial(StoreContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var result = _context.Todos.OrderByDescending(x => x.TodoId).Take(6).ToList();
+        return View(result);
+    }
+}
