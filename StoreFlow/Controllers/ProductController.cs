56b026b7f7b74387792e374f30f25ef319592089
Zehra Seren <fatmazehraseren@gmail.com>
2025-08-11 22:41:52 +0300

    feat(product): implement CRUD operations using direct EF Core methods

---
+++ StoreFlow/Controllers/ProductController.cs
@@ -0,0 +1,116 @@
+﻿using StoreFlow.Models;
+using StoreFlow.Context;
+using StoreFlow.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace StoreFlow.Controllers;
+public class ProductController : Controller
+{
+    private readonly StoreContext _context;
+
+    public ProductController(StoreContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult ProductList()
+    {
+        var products = GetProductsWithCategory();
+        return View(products);
+    }
+
+    [HttpGet]
+    public IActionResult CreateProduct()
+    {
+        LoadCategories();
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CreateProduct(Product product)
+    {
+        _context.Products.Add(product);
+        _context.SaveChanges();
+        return RedirectToAction("ProductList");
+    }
+
+    public IActionResult DeleteProduct(int id)
+    {
+        var product = _context.Products.Find(id);
+        _context.Remove(product);
+        _context.SaveChanges();
+        return RedirectToAction("ProductList");
+    }
+
+    [HttpGet]
+    public IActionResult UpdateProduct(int id)
+    {
+        LoadCategories();
+        var product = _context.Products.Find(id);
+        return View(product);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateProduct(Product product)
+    {
+        _context.Products.Update(product);
+        _context.SaveChanges();
+        return RedirectToAction("ProductList");
+    }
+
+    public IActionResult ListProductsByCategory(int id)
+    {
+        var products = GetProductsWithCategory()
+            .Where(p => p.CategoryId == id)
+            .ToList();
+
+        if (!products.Any()) return NotFound();
+
+        ViewBag.categoryName = products.First().CategoryName;
+
+        return View(products);
+    }
+
+    private void LoadCategories()
+    {
+        ViewBag.categories = _context.Categories
+            .Select(x => new SelectListItem
+            {
+                Text = x.CategoryName,
+                Value = x.CategoryId.ToString()
+            })
+            .OrderBy(x => x.Text)
+            .ToList();
+    }
+
+    private List<ProductListByCategoryViewModel> GetProductsWithCategory()
+    {
+        var product = _context.Products
+            .Include(p => p.Category)
+            .Select(p => new ProductListByCategoryViewModel
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                ProductPrice = p.ProductPrice,
+                ProductStock = p.ProductStock,
+                CategoryId = p.CategoryId,
+                CategoryName = p.Category.CategoryName,
+            }).ToList();
+
+        return product;
+    }
+
+    public IActionResult First5ProductsList()
+    {
+        var products = GetProductsWithCategory().Take(5).ToList();
+        return View(products);
+    }
+
+    public IActionResult Skip4ProductsList()
+    {
+        var products = GetProductsWithCategory().Skip(4).Take(10).ToList();
+        return View(products);
+    }
+}
---
+++ StoreFlow/Models/ProductListByCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StoreFlow.Models;
+
+public class ProductListByCategoryViewModel
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public decimal ProductPrice { get; set; }
+    public int ProductStock { get; set; }
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+}
