63f1e0cabc873e46dc2ab80207f0e5aba1dd230c
Zehra Seren <fatmazehraseren@gmail.com>
2025-08-11 16:34:20 +0300

    feat: add initial layout skeleton

---
+++ StoreFlow/Controllers/LayoutController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreFlow.Controllers;
+public class LayoutController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
