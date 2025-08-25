1aa0f04c7b9541448b390989e38bb79ff02e879e
Zehra Seren <fatmazehraseren@gmail.com>
2025-08-25 23:51:51 +0300

    feat(entity): message entity

---
+++ StoreFlow/Entities/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace StoreFlow.Entities;
+
+public class Message
+{
+    public int MessageId { get; set; }
+    public string MessageTitle { get; set; }
+    public string MessageDetail { get; set; }
+    public string SenderNameSurname { get; set; }
+    public DateTime DateTime { get; set; }
+    public bool IsRead { get; set; }
+}
