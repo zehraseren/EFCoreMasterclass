fb7eacabbd34e1d372519243d79d9fd526fe345e
Zehra Seren <fatmazehraseren@gmail.com>
2025-08-27 22:42:20 +0300

    feat(dashboard): add Last5Messages and Last5Products components using EF Core

---
+++ StoreFlow/ViewComponents/DashboardComponents/_Last5MessagesComponentPartial.cs
@@ -0,0 +1,24 @@
+﻿using StoreFlow.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreFlow.ViewComponents.DashboardComponents;
+
+public class _Last5MessagesComponentPartial : ViewComponent
+{
+    private readonly StoreContext _context;
+
+    public _Last5MessagesComponentPartial(StoreContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var messages = _context.Messages
+            .OrderBy(m => m.MessageId)
+            .ToList()
+            .TakeLast(5)
+            .AsEnumerable();
+        return View(messages);
+    }
+}
---
+++ StoreFlow/ViewComponents/DashboardComponents/_Last5ProductsComponentPartial.cs
@@ -0,0 +1,25 @@
+﻿using StoreFlow.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreFlow.ViewComponents.DashboardComponents;
+
+public class _Last5ProductsComponentPartial : ViewComponent
+{
+    private readonly StoreContext _context;
+
+    public _Last5ProductsComponentPartial(StoreContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var products = _context.Products
+            .OrderBy(p => p.ProductId)
+            .AsEnumerable() // EF sorgusunu SQL'den getir, sonra LINQ-to-Objects
+            .SkipLast(5)
+            .TakeLast(6)
+            .ToList();
+        return View(products);
+    }
+}
