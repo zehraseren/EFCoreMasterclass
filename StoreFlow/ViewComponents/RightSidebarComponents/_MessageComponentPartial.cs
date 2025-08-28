f267d572394ce92a751578d8f0ed6d956b343a63
Zehra Seren <fatmazehraseren@gmail.com>
2025-08-28 16:14:13 +0300

    feat(rightsidebar): add Message and TodoList partial components

---
+++ StoreFlow/ViewComponents/RightSidebarComponents/_MessageComponentPartial.cs
@@ -0,0 +1,22 @@
+﻿using StoreFlow.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreFlow.ViewComponents.RightSidebarComponents;
+
+public class _MessageComponentPartial : ViewComponent
+{
+    private readonly StoreContext _context;
+
+    public _MessageComponentPartial(StoreContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var messages = _context.Messages
+            .Where(m => m.IsRead == false)
+            .ToList();
+        return View(messages);
+    }
+}
---
+++ StoreFlow/ViewComponents/RightSidebarComponents/_TodoListComponentPartial.cs
@@ -0,0 +1,24 @@
+﻿using StoreFlow.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreFlow.ViewComponents.RightSidebarComponents;
+
+public class _TodoListComponentPartial : ViewComponent
+{
+    private readonly StoreContext _context;
+
+    public _TodoListComponentPartial(StoreContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var result = _context.Todos
+            .OrderBy(t => t.TodoId)
+            .AsEnumerable()
+            .TakeLast(10)
+            .ToList();
+        return View(result);
+    }
+}
